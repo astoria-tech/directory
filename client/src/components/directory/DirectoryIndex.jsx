3c7e0c764f752a37e3eb4ba67614d6225a6506e1
Peter Valdez <peter@nycmesh.net>
2019-09-11 14:37:36 -0400

    Add the Sept meetup date

--- client/src/components/directory/DirectoryIndex.jsx
+++ client/src/components/directory/DirectoryIndex.jsx
@@ -112,10 +112,8 @@ class DirectoryIndex extends Component {
                   <hr className={classes.divider} />
 
                   <Typography className={classes.link} variant="h5" color="textPrimary">
-                    <Link href="https://meetup.astoria.app/">
                       Next Meetup:<br/>
-                      Aug 28th @ The Strand
-                    </Link>
+                      Sept 25th @ The Strand
                   </Typography>
 
                   <hr className={classes.shortDivider} />
