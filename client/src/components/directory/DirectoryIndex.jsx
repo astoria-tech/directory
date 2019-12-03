e90453dd9aec53f477c9a27518bc2ba277bd185c
Peter Valdez <peter@nycmesh.net>
2019-12-03 17:46:04 -0500

    Add December meetup date/link

--- client/src/components/directory/DirectoryIndex.jsx
+++ client/src/components/directory/DirectoryIndex.jsx
@@ -111,9 +111,9 @@ class DirectoryIndex extends Component {
                   <hr className={classes.divider} />
 
                   <Typography className={classes.link} variant="h5" color="textPrimary">
-                    <Link href="https://www.meetup.com/Astoria-Tech-Meetup/events/265892329/">
-                      Monthly Meetup #14:<br/>
-                      Weds, Nov 20th<br/>
+                    <Link href="https://www.meetup.com/Astoria-Tech-Meetup/events/266668634/">
+                      Monthly Meetup #15:<br/>
+                      Weds, Dec 18th<br/>
                       @ The Strand Smokehouse
                     </Link>
                   </Typography>
